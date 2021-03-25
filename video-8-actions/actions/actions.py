a005283a92bdc9eb36d13a2384734b3ae5f72b06
koaning <vincentwarmerdam@gmail.com>
2021-03-25 17:00:48 +0100

    added-first-action

---
+++ video-8-actions/actions/actions.py
@@ -0,0 +1,41 @@
+from typing import Any, Text, Dict, List
+
+import arrow 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+city_db = {
+    'brussels': 'Europe/Brussels', 
+    'zagreb': 'Europe/Zagreb',
+    'london': 'Europe/Dublin',
+    'lisbon': 'Europe/Lisbon',
+    'amsterdam': 'Europe/Amsterdam',
+    'seattle': 'US/Pacific'
+}
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_tell_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_place = next(tracker.get_latest_entity_values("place", None))
+        utc = arrow.utcnow()
+        
+        if not current_place:
+            msg = f"It's {utc.format('HH:mm')} utc now. You can also give me a place."
+            dispatcher.utter_message(text=msg)
+            return []
+        
+        tz_string = city_db.get(current_place, None)
+        if not tz_string:
+            msg = f"It's I didn't recognize {current_place}. Is it spelled correctly?"
+            dispatcher.utter_message(text=msg)
+            return []
+        
+        msg = f"It's {utc.to(city_db[current_place]).format('HH:mm')} in {current_place} now."
+        dispatcher.utter_message(text=msg)
+        
+        return []
