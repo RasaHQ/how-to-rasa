ba2697d7c5e2712337eddb754ddb041684baf3e9
koaning <vincentwarmerdam@gmail.com>
2021-04-07 17:24:17 +0200

    itworks

--- video-09-forms/actions/actions.py
+++ video-09-forms/actions/actions.py
@@ -1,16 +1,100 @@
 from typing import Text, List, Any, Dict
 
-from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
 ALLOWED_PIZZA_SIZES = ["small", "medium", "large", "extra-large", "extra large", "s", "m", "l", "xl"]
 ALLOWED_PIZZA_TYPES = ["mozzarella", "fungi", "veggie", "pepperoni", "hawaii"]
+VEGETARIAN_PIZZAS = ["mozzarella", "fungi", "veggie"]
 
 
 class ValidateSimplePizzaForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_simple_pizza_form"
+
+    def validate_pizza_size(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `pizza_size` value."""
+
+        if slot_value not in ALLOWED_PIZZA_SIZES:
+            dispatcher.utter_message(text=f"We only accept pizza sizes: s/m/l/xl.")
+            return {"pizza_size": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        return {"pizza_size": slot_value}
+
+    def validate_pizza_type(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `pizza_type` value."""
+
+        if slot_value not in ALLOWED_PIZZA_TYPES:
+            dispatcher.utter_message(text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
+            return {"pizza_type": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        return {"pizza_type": slot_value}
+
+
+class AskForVegetarianAction(Action):
+    def name(self) -> Text:
+        return "action_ask_vegetarian"
+
+    def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(text="Would you like to order a vegetarian pizza?",
+                                 buttons=[
+                                     {"name": "yes", "payload": "/affirm"},
+                                     {"name": "no", "payload": "/deny"}
+                                 ])
+        return []
+
+
+class AskForPizzaTypeAction(Action):
+    def name(self) -> Text:
+        return "action_ask_pizza_type"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        if tracker.get_slot("vegetarian"):
+            dispatcher.utter_message(text=f"What kind of pizza do you want?",
+                                     buttons=[{"name": p, "payload": p} for p in VEGETARIAN_PIZZAS])
+        else:
+            dispatcher.utter_message(text=f"What kind of pizza do you want?")
+        return []
+
+
+class ValidateFancyPizzaForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_fancy_pizza_form"
+
+    def validate_vegetarian(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `pizza_size` value."""
+        if tracker.get_intent_of_latest_message() == "affirm":
+            dispatcher.utter_message(text="I'll remember you prefer vegetarian.")
+            return {"vegetarian": True}
+        if tracker.get_intent_of_latest_message() == "deny":
+            dispatcher.utter_message(text="I'll remember that.")
+            return {"vegetarian": True}
+        dispatcher.utter_message(text="I didn't get that.")
+        return {"vegetarian": None}
 
     def validate_pizza_size(
         self,
